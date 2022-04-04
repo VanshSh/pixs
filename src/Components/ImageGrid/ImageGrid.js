220c8ce9265e88c76284e1ac6642694ff7a7f774
Vansh Sharma <vanshsharma9354@gmail.com>
2022-04-04 15:12:04 +0530

    Remove the div

--- src/Components/ImageGrid/ImageGrid.js
+++ src/Components/ImageGrid/ImageGrid.js
@@ -1,37 +1,37 @@
-import React from 'react'
-import useFirestore from '../../Hooks/useFirestore'
-import { motion } from 'framer-motion'
-import ImageList from '@mui/material/ImageList'
-import ImageListItem from '@mui/material/ImageListItem'
+import React from "react";
+import useFirestore from "../../Hooks/useFirestore";
+import { motion } from "framer-motion";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const ImageGrid = ({ onSelectImg }) => {
-    const { docs } = useFirestore('images')
+  const { docs } = useFirestore("images");
 
-    return (
-        <>
-        div.
-            <ImageList
-                sx={{ width: 500, height: 450 }}
-                cols={3}
-                rowHeight={164}
-                gap={8}
-                variant='quilted'
-            >
-                {docs.map((item) => (
-                    <ImageListItem key={item.id}>
-                        <img
-                            onClick={() => onSelectImg(item.url)}
-                            src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.url}
-                            sc
-                            loading='lazy'
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </>
-    )
-}
+  return (
+    <>
+      <motion.div className="img_grid" layout>
+        <ImageList
+          sx={{ width: "100vw", height: 450 }}
+          cols={3}
+          rowHeight={164}
+          gap={10}
+          variant="quilted"
+        >
+          {docs.map((item) => (
+            <ImageListItem key={item.id}>
+              <img
+                onClick={() => onSelectImg(item.url)}
+                src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.url}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </motion.div>
+    </>
+  );
+};
 
-export default ImageGrid
+export default ImageGrid;
